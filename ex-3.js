5746a9d9c35fa45376d6f8df471c5a8885bcce45
James <Natasan2121@gmail.com>
2023-10-15 21:37:42 +0700

    complete equality-of-values assignment

--- ex-3.js
+++ ex-3.js
@@ -1,12 +1,18 @@
 function isPlainObject(value) {
-  // Start coding here
+  if (typeof value == "object" && Array.isArray(value) == false && value != null){
+    return true
+  }else if (typeof value == "object" && Array.isArray(value) == true){
+    return false
+  }else if (value == null){
+    return false
+  }else return "“This is not an object”"
 }
 
 // Example case
 let result1 = isPlainObject({ name: "John" });
 let result2 = isPlainObject(null);
 let result3 = isPlainObject(["apples", "oranges"]);
-
+let a = typeof { name: "John" }
 // ผลลัพธ์ควรที่ได้จาก Example case
 console.log(result1); //true
 console.log(result2); // false
